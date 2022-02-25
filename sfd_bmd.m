8e3b1ac9729302a58b2c1f620806086ec14341c5
Md Kaiser Hamid Munna <81263287+kaiser-75@users.noreply.github.com>
2022-02-25 14:55:40 +0600

    Update sfd_bmd.m

--- sfd_bmd.m
+++ sfd_bmd.m
@@ -1,11 +1,12 @@
 clear 
 clc
+%% notification for user
 noe=input('How many element in beams ? (including reaction force) ');
 l=input('Enter length of your beam ');
 
 disp('Enter as vector: [load type,starting position,ending position,starting magnitude,ending magnitude');
 
-disp('1. Enter Type of load.For Point Load=0; UDL=1;Ramp load=2 ');
+disp('1. Enter Type of load.For Point Load=0;UDL=1 Ramp load=2, Concenrated moment=3 ');
 
 disp('2. Enter start position of element ');
 disp('3. Enter ending posit-ion of element ');
@@ -13,28 +14,44 @@ disp('3. Enter ending posit-ion of element ');
 disp('4. Enter start load magnitude ');
 disp('5. Enter ending load magnitude ');
 
-disp('If it is point load, then follow [1,2,4] ');
-disp('If ramp load left value greater than right value then put a negative sign in magnitude');
+disp('Special case: If it is point load/concenrated moment, then follow [1,2,4] ');
 disp('For downward load put a negative sign');
+disp('For clockwise concentrated moment put a negative sign [load_type, position,value]');
 
-% Calculation
+%% Calculation
 value=zeros(1,l*100+1);
 moment=zeros(1,l*100+1);
-for i=1:noe
+
+
+ for i=1:noe
     % element of beam vector
     loadvector=input('Enter load vector : \n');
 
     x=loadvector(1,2); %position of element
-    if(loadvector(1,1)==0) %point load
+    if(loadvector(1,1)==0 || loadvector(1,1)==3) %point load
         c=int16(l*100+1)-x*100;
       for j=1:c
           p=int16(x*100)+1;
-        value(1,p)=value(1,p)+loadvector(1,3)*beam(x,loadvector(1,2),loadvector(1,1));
-        moment(1,p)=moment(1,p)+loadvector(1,3)*beam(x,loadvector(1,2),loadvector(1,1)+1);
-        x=x+0.01;
-       end
+          if loadvector(1,1)==3
+              moment(1,p)=moment(1,p)+ loadvector(1,3)*beam(x,loadvector(1,2),loadvector(1,1)-3);
+          else
+           value(1,p) = value(1,p)+loadvector(1,3)*beam(x,loadvector(1,2),loadvector(1,1));
+           moment(1,p) = moment(1,p)-loadvector(1,3)*beam(x,loadvector(1,2),loadvector(1,1)+1);
+        
+          end
+           x=x+0.01;
+      end
 
-    else %ramp
+    elseif loadvector(1,1)==1
+                  c=int16(l*100+1)-x*100;
+      for j=1:c
+           p=int16(x*100)+1;
+           value(1,p)=value(1,p)+loadvector(1,4)*beam(x,loadvector(1,2),loadvector(1,1))-loadvector(1,5)*beam(x,loadvector(1,3),loadvector(1,1));
+           moment(1,p)=moment(1,p)-loadvector(1,4)/2*beam(x,loadvector(1,2),loadvector(1,1)+1)+loadvector(1,5)/2*beam(x,loadvector(1,3),loadvector(1,1)+1);
+           x=x+0.01;
+       end
+    
+    else %ramp load==2
         c=int16(l*100+1)-x*100;
         for j=1:c
             p=int16(x*100)+1;
@@ -43,18 +60,23 @@ for i=1:noe
             ps=loadvector(1,3)-loadvector(1,2);
             slope=ld/ps;
 
-        value(1,p)=value(1,p)+loadvector(1,4)*beam(x,loadvector(1,2),(loadvector(1,1)-1))-loadvector(1,4)*beam(x,loadvector(1,3),(loadvector(1,1))-1)+slope/2*beam(x,loadvector(1,2),loadvector(1,1))-slope/2*beam(x,loadvector(1,3),loadvector(1,1));
-        moment(1,p)=moment(1,p)+loadvector(1,4)/2*beam(x,loadvector(1,2),(loadvector(1,1)))-loadvector(1,4)/2*beam(x,loadvector(1,3),(loadvector(1,1)))+slope/6*beam(x,loadvector(1,2),(loadvector(1,1)+1))-slope/6*beam(x,loadvector(1,3),(loadvector(1,1)+1));
-        x=x+0.01;
-        end
-    end
-end
-      x=0:0.01:l;
+          value(1,p)=value(1,p)-loadvector(1,4)*beam(x,loadvector(1,2),(loadvector(1,1)-1))-loadvector(1,5)*beam(x,loadvector(1,3),(loadvector(1,1))-1)+slope/2*beam(x,loadvector(1,2),loadvector(1,1))-slope/2*beam(x,loadvector(1,3),loadvector(1,1));
+          moment(1,p)=moment(1,p)+loadvector(1,4)/2*beam(x,loadvector(1,2),(loadvector(1,1)))+loadvector(1,5)/2*beam(x,loadvector(1,3),(loadvector(1,1)))-slope/6*beam(x,loadvector(1,2),(loadvector(1,1)+1))+slope/6*beam(x,loadvector(1,3),(loadvector(1,1)+1));
+          x=x+0.01;
+         end
+     end
+  end
+
+%% plottting sfd bmd
+     x=0:0.01:l;
      subplot(2,1,1);
      area(x,value,'faceColor','g');
+     title('Shear Force Diagram')
      subplot(2,1,2);
      area(x,moment,'faceColor','b');
+     title('Bending Moment Diagram')
 
+ %% Code done by Kaiser(1804083) %%
 
 
 
